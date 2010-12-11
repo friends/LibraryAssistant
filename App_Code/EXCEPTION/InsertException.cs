c1577cbcb01864432e22a42147b09cae7f917d0a
xxj <xxj050@163.com>
2010-12-11 14:44:56 +0800

    add to the class file with author infomation and review time

--- App_Code/EXCEPTION/InsertException.cs
+++ App_Code/EXCEPTION/InsertException.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//   InsertException class version 1.0
+//   ==================================================
+//   Copyright (C) 2010  Xie Xiaojing
+//   作者： 谢晓境 
+//   Email: xxj050@163.com
+//   最后修改时间： 2010 - 12 - 6
+//   检查者: 陈兵
+//   最后检查时间: 2010 - 12 -6
+//   ==================================================
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,15 +16,13 @@ using System.Web;
 namespace LAS.Exceptions
 {
     /// <summary>
-    ///InsertException 的摘要说明
+    /// 数据库插入异常类
     /// </summary>
     public class InsertException : DatabaseOperationException
     {
         public InsertException()
         {
-            //
-            //TODO: 在此处添加构造函数逻辑
-            //
+            // do nothing
         }
     }
 }
