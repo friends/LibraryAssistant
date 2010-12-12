82945b642d548191d069d2415f4d8d2c38581e23
xxj <xxj050@163.com>
2010-12-12 17:11:23 +0800

    modify after review

--- administor/NewsEdit.aspx.cs
+++ administor/NewsEdit.aspx.cs
@@ -4,7 +4,7 @@
 //   作者： 谢晓境
 //   Email: xxj050@163.com
 //   最后修改时间： 2010 - 12 - 11
-//   检查者: 陈兵
+//   检查者: 苏平
 //   最后检查时间: 2010 - 12 -11
 //   =================================
 
@@ -24,14 +24,31 @@ using System.Net;
 using System.Text.RegularExpressions;
 using DT;
 using LAS.DAL;
+using LAS.Exceptions;
 
 public partial class admin_postedit : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
     }
-
+   
     protected void btnEdit_Click(object sender, EventArgs e)
+    {        
+        try
+        {
+            insertNews();
+            PromptSuccessMessage();
+        }
+        catch (InsertException insertException)
+        {
+            PromptErrorMessage(insertException.Message);
+        }
+    }
+
+    /// <summary>
+    /// 插入新闻 
+    /// </summary>
+    public void insertNews()
     {
         News news = new News();
         news.content = content.Value;
@@ -40,7 +57,25 @@ public partial class admin_postedit : Page
         news.publishTime = DateTime.Now;
         NewsHandler newsHandler = new NewsHandler(news);
         newsHandler.Insert();
-        Response.Write("<script>alert('添加成功!')</script>");
+    }
+
+    /// <summary>
+    /// 提示成功信息
+    /// </summary>
+    public void PromptSuccessMessage()
+    {
+        Response.Write("<script>alert('添加成功!');</script>");
         Response.Redirect("NewsManage.aspx");
+    }
+
+
+    /// <summary>
+    /// 提示错误信息
+    /// </summary>
+    /// <param name="errorMessage">错误消息</param>
+    public void PromptErrorMessage(string errorMessage)
+    {
+        Response.Write("<script>alert('添加失败!" + errorMessage + "');</script>");
+        Response.Redirect("NewsEdit.aspx");
     }
 }
